63a1438bba100b2f853c81a1e150cfa85fb5bbf9
DAW-DUCKASWELL <santiagomuchacholuma@uvm.edu.ve>
2023-09-30 16:23:28 -0600

    Botón de editar
    Botón de editar agregado

---
+++ src/components/TaskEditor.js
@@ -0,0 +1,6 @@
+export const EditTask = (task) => {
+    setTask([
+        task, 
+        task
+    ])
+};
--- src/components/TaskRow.js
+++ src/components/TaskRow.js
@@ -1,12 +1,18 @@
+import '../btn.css'
 export const TaskRow = ({ task, toggleTask }) => (
   <tr key={task.name}>
-    <td className="d-flex justify-content-between">
+    <td className="d-flex justify-content-between td-task">
       {task.name}
-      <input
-        type="checkbox"
-        checked={task.done}
-        onChange={() => toggleTask(task)}
-      />
+      <div className='div-checkEdit'>
+        <button className="edit-btn">
+          Edit
+        </button>
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={() => toggleTask(task)}
+        />
+      </div>
     </td>
   </tr>
 );
